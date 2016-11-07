504781a40b91f4ee4b3d8617ae3a4cbcdb214668
zhangzhifeng <837856614@qq.com>
2016-11-08 07:55:20 +0800

    使用MJRefresh，添加下拉刷新和上拉加载功能

--- JFQDaily/ViewControllers/JFHomeViewController.m
+++ JFQDaily/ViewControllers/JFHomeViewController.m
@@ -10,12 +10,12 @@
 
 #import "JFConfigFile.h"
 #import <Masonry.h>
+#import <MJRefresh.h>
 #import "MJExtension.h"
 #import "JFWindow.h"
 #import "JFHomeNewsDataManager.h"
 #import "JFHomeNewsTableViewCell.h"
 #import "JFLoopView.h"
-
 //新闻数据模型相关
 #import "JFResponseModel.h"
 #import "JFFeedsModel.h"
@@ -27,15 +27,23 @@
 static NSString *ID = @"newsCell";
 
 @interface JFHomeViewController ()<UITableViewDelegate, UITableViewDataSource>
+{
+    NSString *_last_key;//上拉加载时传入的key
+    NSString *_has_more;//是否还有未加载的文章
+}
 
 @property (nonatomic, strong) UITableView *homeNewsTableView;
 @property (nonatomic, strong) JFHomeNewsTableViewCell *cell;
 @property (nonatomic, strong) JFWindow *jfWindow;
 @property (nonatomic, strong) JFHomeNewsDataManager *manager;
 @property (nonatomic, strong) JFResponseModel *response;
-@property (nonatomic, strong) NSMutableArray *feedsArray;
-@property (nonatomic, strong) NSMutableArray *bannersArray;
-@property (nonatomic, strong) NSMutableArray *imageMutableArray;
+@property (nonatomic, strong) NSArray *feedsArray;
+@property (nonatomic, strong) NSArray *bannersArray;
+@property (nonatomic, strong) NSArray *imageArray;
+/** 主要内容数组*/
+@property (nonatomic, strong) NSMutableArray *contentMutableArray;
+@property (nonatomic, strong) MJRefreshNormalHeader *refreshHeader;
+@property (nonatomic, strong) MJRefreshAutoNormalFooter *refreshFooter;
 
 @end
 
@@ -43,8 +51,45 @@ static NSString *ID = @"newsCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.imageMutableArray = [[NSMutableArray alloc] init];
+    self.contentMutableArray = [[NSMutableArray alloc] init];
+    self.imageArray = [[NSArray alloc] init];
     [self.manager requestHomeNewsDataWithLastKey:@"0"];
+    
+    //设置下拉刷新
+    self.refreshHeader = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        
+        [self refreshData];
+    }];
+    self.refreshHeader.lastUpdatedTimeLabel.hidden = YES;
+    self.refreshHeader.stateLabel.hidden = YES;
+    self.homeNewsTableView.mj_header = self.refreshHeader;
+    
+    //设置上拉加载
+    self.refreshFooter = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+        [self loadData];
+    }];
+    [self.refreshFooter setTitle:@"加载更多 ..." forState:MJRefreshStateRefreshing];
+    [self.refreshFooter setTitle:@"没有更多内容了" forState:MJRefreshStateNoMoreData];
+    self.homeNewsTableView.mj_footer = self.refreshFooter;
+    
+}
+
+/// 下拉刷新数据
+- (void)refreshData {
+    //下拉刷新时清空数据
+    self.contentMutableArray = [[NSMutableArray alloc] init];
+//    self.homeNewsTableView.tableHeaderView = nil;
+    [self.manager requestHomeNewsDataWithLastKey:@"0"];
+}
+
+/// 上拉加载数据
+- (void)loadData {
+    //判断是否还有更多数据
+    if ([_has_more isEqualToString:@"1"]) {
+        [self.manager requestHomeNewsDataWithLastKey:_last_key];
+    }else if ([_has_more isEqualToString:@"0"]) {
+    [self.refreshFooter setState:MJRefreshStateNoMoreData];
+    }
 }
 
 - (void)loadView {
@@ -56,17 +101,18 @@ static NSString *ID = @"newsCell";
 
 /// 添加图片轮播器
 - (void)addLoopView {
-    
-    NSMutableArray *imageMuatableArray = [[NSMutableArray alloc] init];
-    NSMutableArray *titleMutableArray = [[NSMutableArray alloc] init];
-    for (JFBannersModel *banner in self.bannersArray) {
-        [imageMuatableArray addObject:banner.post.image];
-        [titleMutableArray addObject:banner.post.title];
+    //这里做判断是不免上拉加载时出现数据错误（如果轮播器已经存在就不再添加）
+    if (!self.homeNewsTableView.tableHeaderView) {
+        NSMutableArray *imageMuatableArray = [[NSMutableArray alloc] init];
+        NSMutableArray *titleMutableArray = [[NSMutableArray alloc] init];
+        for (JFBannersModel *banner in self.bannersArray) {
+            [imageMuatableArray addObject:banner.post.image];
+            [titleMutableArray addObject:banner.post.title];
+        }
+        JFLoopView *loopView = [[JFLoopView alloc] initWithImageMutableArray:imageMuatableArray titleMutableArray:titleMutableArray];
+        loopView.frame = CGRectMake(0, 0, JFSCREEN_WIDTH, 300);
+        self.homeNewsTableView.tableHeaderView = loopView;
     }
-    
-    JFLoopView *loopView = [[JFLoopView alloc] initWithImageMutableArray:imageMuatableArray titleMutableArray:titleMutableArray];
-    loopView.frame = CGRectMake(0, 0, JFSCREEN_WIDTH, 300);
-    self.homeNewsTableView.tableHeaderView = loopView;
 }
 
 
@@ -77,9 +123,21 @@ static NSString *ID = @"newsCell";
         __weak typeof(self) weakSelf = self;
         [_manager newsDataBlock:^(id data) {
             weakSelf.response = [JFResponseModel mj_objectWithKeyValues:data];
+            _last_key = weakSelf.response.last_key;
+            _has_more = weakSelf.response.has_more;
+            
+            
             weakSelf.feedsArray = [JFFeedsModel mj_objectArrayWithKeyValuesArray:[data valueForKey:@"feeds"]];
+            
+            //在contentMutableArray后面添加一个数组
+            [weakSelf.contentMutableArray addObjectsFromArray:weakSelf.feedsArray];
+            
             weakSelf.bannersArray = [JFFeedsModel mj_objectArrayWithKeyValuesArray:[data valueForKey:@"banners"]];
-#warning 不应该在这里添加，需要优化
+            
+            //停止刷新
+            [self.refreshHeader endRefreshing];
+            [self.refreshFooter endRefreshing];
+            
             [self addLoopView];
             [self.homeNewsTableView reloadData];
         }];
@@ -101,11 +159,11 @@ static NSString *ID = @"newsCell";
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.feedsArray.count;
+    return self.contentMutableArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    JFFeedsModel *feed = self.feedsArray[indexPath.row];
+    JFFeedsModel *feed = self.contentMutableArray[indexPath.row];
     self.cell = [tableView cellForRowAtIndexPath:indexPath];
     if (self.cell == nil) {
         self.cell = [[JFHomeNewsTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
@@ -116,6 +174,7 @@ static NSString *ID = @"newsCell";
         self.cell.commentCount = [NSString stringWithFormat:@"%ld",(long)feed.post.comment_count];
         self.cell.praiseCount = [NSString stringWithFormat:@"%ld",(long)feed.post.praise_count];
     }
+    self.cell.selectionStyle = UITableViewCellSelectionStyleNone;
     if (![feed.type isEqualToString:@"1"]) {
         self.cell.subhead = feed.post.subhead;
     }
